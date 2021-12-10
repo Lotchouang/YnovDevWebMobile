e52c2f6f9e5cbac0e228a00469a756267aaca309
styve <lotchouang33@yahoo.fr>
2021-12-10 09:24:02 +0100

    message push

--- www/sw.js
+++ www/sw.js
@@ -1,6 +1,29 @@
 /** VARS */
-const CACHE_NAME = "offline";
+const CACHE_NAME = "offline-v6";
 const OFFLINE_URL = "offline.html";
+const ORIGIN_URL = `${location.protocol}//${location.host}`;
+const CACHED_FILES = [
+  OFFLINE_URL,
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js",
+  `${ORIGIN_URL}/css/index.css`,
+  `${ORIGIN_URL}/js/index.js`,
+  `${ORIGIN_URL}/img/logo.png`,
+];
+
+
+const deleteOldCaches = () =>
+  new Promise((resolve) => {
+    caches.keys().then((keys) => {
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            caches.delete(key);
+          }
+        })
+      ).finally(resolve);
+    });
+  });
 
 /** FUNCTIONS */
 
@@ -37,13 +60,16 @@ const respondWithFetchPromiseNavigate = (event) =>
       });
   });
 
-const fetchSW = (event) => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  if (event.request.mode === "navigate") {
-    event.respondWith(respondWithFetchPromiseNavigate(event));
-  }
-};
+  const fetchSW = (event) => {
+    // We only want to call event.respondWith() if this is a navigation request
+    // for an HTML page.
+    if (event.request.mode === "navigate") {
+      event.respondWith(respondWithFetchPromiseNavigate(event));
+    } else if (CACHED_FILES.includes(event.request.url)) {
+      event.respondWith(caches.match(event.request));
+    }
+  };
+  
 
 /*********************************** */
 
@@ -51,12 +77,10 @@ const fetchSW = (event) => {
 
 const waitUntilActivatePromise = () =>
   new Promise((resolve) => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
-    if ("navigationPreload" in self.registration) {
-      self.registration.navigationPreload.enable().finally(resolve);
-    }
-  });
+    deleteOldCaches().then(() => {
+      if ("navigationPreload" in self.registration) {
+        self.registration.navigationPreload.enable().finally(resolve);}});});
+
 
 const activate = (event) => {
   event.waitUntil(waitUntilActivatePromise());
@@ -70,9 +94,10 @@ const activate = (event) => {
 const waitUntilInstallationPromise = () =>
   new Promise((resolve) => {
     caches.open(CACHE_NAME).then((cache) => {
-      cache.add(new Request(OFFLINE_URL, { cache: "reload" })).then(resolve);
+      cache.addAll(CACHED_FILES).then(resolve);
     });
   });
+
 
 const installSW = (event) => {
   event.waitUntil(waitUntilInstallationPromise());
